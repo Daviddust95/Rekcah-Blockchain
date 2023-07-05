e08fe66f55ba91f04e01406d6fc99dbb0ccc5551
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-05 17:05:37 -0300

    Update block.js

--- minha_primeira_blockchain/block.js
+++ minha_primeira_blockchain/block.js
@@ -40,7 +40,7 @@ let nonce = 0;
 do {
 nonce++;
 timestamp = Date.now()
-
+difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp}) ;
 hash = cryptoHash(timestamp, lastHash, difficulty, nonce)
 } 
 while (hexToBinary(hash).substring(0,difficulty))
@@ -52,11 +52,13 @@ return new this (timestamp, lastHash, data, difficulty, nonce, hash)
 
 static adjustDifficulty({originalBlock, timestamp}) {
 
-const (difficulty) = originalBlock;
+const {difficulty} = originalBlock;
 
 if (difficulty < 1) return 1;
+
 if ((timestamp - originalBlock.timestamp)>MINE_RATE) return difficulty -1 ;
 
+return difficulty + 1
 }
 
 }
