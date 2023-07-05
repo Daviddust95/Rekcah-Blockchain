911dd9a79594722d48ab51dd28096aba9178ef8a
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-05 18:29:10 -0300

    Criando a classe Blockchain

--- minha_primeira_blockchain/index.js
+++ minha_primeira_blockchain/index.js
@@ -9,9 +9,16 @@ this.chain =[Block.genesis()];
 
 
 }
+
 addBlock({}) {
 
+const newBlock = Block.mineBlock ({
+lastBlock: this.chain.chain[this.chain.length -1],
+data
+})
 
+
+this.chain.push(newBlock)
 }
 
 }
