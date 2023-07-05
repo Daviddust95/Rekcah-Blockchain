384168ec100225e40f6c9d4cd483a1d36b0ac558
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-05 09:34:05 -0300

    .

--- minha_primeira_blockchain/block.js
+++ minha_primeira_blockchain/block.js
@@ -7,7 +7,7 @@
 //Hash é uma síntese      função hash  >>
 
 // quantidade maior de zeros = dificuldade de mineração do algoritmo
-
+const {GENESIS_DATA} = require{'../'}
 class Block {
 
     constructor({ data, hash, nonce, difficulty, timestamp, lasthash }) { 
---
+++ minha_primeira_blockchain/config.js
@@ -0,0 +1,17 @@
+
+
+const MINING_REWARD = 50;
+
+const INITIAL_DIFFICULTY = 3;
+
+const STARTING_BALANCE = 1000;
+
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    lasthash: '----------',
+    hash: "abcde",
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
+    data: []
+};
