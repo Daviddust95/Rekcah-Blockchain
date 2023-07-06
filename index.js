fe92a0293910802d9ad22dba1ec42cd58be51e1c
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-06 01:14:46 -0300

    Construindo o método POST da Blockchain API

--- index.js
+++ index.js
@@ -13,5 +13,15 @@ res.json(blockchain.chain);
 
 })
 
+app.post ('/api/mine',(req,res)=>{
+
+const {data} = req.body;
+
+blockchain.addBlock((data))
+
+res.redirect('/api/blocks')
+
+})
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening at localhost: ${PORT}`));
